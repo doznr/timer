7c90ef2824eed6055048b39fa18c982eb5ba7913
doznr <111475219+doznr@users.noreply.github.com>
2023-07-19 11:20:17 +0900

    Add files via upload

---
+++ js/share.js
@@ -0,0 +1,33 @@
+const url = 'https://smartstore.naver.com/jhelectric/products/5448674722';
+
+function setShare(){
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '서준전기 타이머콘센트 결과';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-' + resultAlt + '.jpg';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
+  
+  more.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDes,
+      imageUrl: shareImage,
+      link: {
+        mobileWebUrl: shareURL,
+        webUrl: shareURL
+      },
+    },
+
+    buttons: [
+      {
+        title: '결과확인하기',
+        link: {
+          mobileWebUrl: shareURL,
+          webUrl: shareURL,
+        },
+      },
+    ]
+  });
+}
